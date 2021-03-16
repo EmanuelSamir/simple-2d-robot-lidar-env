8932391e8c5b7bfb0cda3c676a1d46ea01df3cda
Raúl Escandón <elmer.escandon@utec.edu.pe>
2021-03-15 19:36:07 -0500

    Update Robot2D Env v0.0.1

--- gym-robot2d/gym_robot2d/envs/robot2d_env.py
+++ gym-robot2d/gym_robot2d/envs/robot2d_env.py
@@ -18,7 +18,7 @@ from gym.utils import seeding
 
 
 class Robot2d(gym.Env): 
-	def __init__(self):
+	def __init__(self, x_goal=4, y_goal=4):
 		"""
 		Description: 
 			A 2D Robot is placed on an empty environment
@@ -28,16 +28,15 @@ class Robot2d(gym.Env):
 
 		Observation: 
 			Type: Box (5)
-			Num 	Observation 	Min 		Max
-			0  		X Position		-5			5
-			1 		X Velocity		-Inf		Inf
-			2 		Y position 		-5 			5
-			3		Y Velocity 		-Inf		Inf
+			Num 	Observation 	Min 	Max
+			0  		X Position		-5		5
+			1 		X Velocity		-5		5
+
 		Action: 
 			Type: Box (2)
 			Num 	Action 	 Min 	Max
-			0	 	Acc_x	 -Inf	Inf
-			1 		Acc_y    -Inf 	Inf
+			0	 	Vx	 -Inf	Inf
+			1 		Vy    -Inf 	Inf
 		"""
 
 		super(Robot2d, self).__init__()
@@ -46,42 +45,58 @@ class Robot2d(gym.Env):
 		self.state = None
 		self.viewer = None 
 		self.robot = Robot()
+		self.eps_err = 0.05
+		self.steps = 0
+
+		# Position variables
+		self.x_goal = x_goal 
+		self.y_goal = y_goal 
+		self.robot_goal = np.array([self.x_goal, self.y_goal])
 
 		# Spaces Environment
-		low = np.array([-THRESHOLD_MAP_X, 
-						-np.finfo(np.float32).max,
-						-THRESHOLD_MAP_Y,
-						-np.finfo(np.float32).max],dtype=np.float32) # Add min box 00 points of lidar
-
-		high = np.array([THRESHOLD_MAP_X, 
-						np.finfo(np.float32).max,
-						THRESHOLD_MAP_Y,
-						np.finfo(np.float32).max],dtype=np.float32) # Add máx 90 points of lidar
-
-		self.observation_space = spaces.Box(low, high, dtype=np
-			.float32)
-
-		self.action_space = spaces.Box(low = -np.finfo(np.float32).max, high = np.finfo(np.float32).max, shape = (2,1), dtype= np.float32)
-
+		self.observation_space = spaces.Box(low = self.robot.env_min_size, 
+			high = self.robot.env_max_size, shape = (2,1), dtype=np.float32)
+		self.action_space = spaces.Box(low = -np.finfo(np.float32).max, 
+			high = np.finfo(np.float32).max, shape = (2,1), dtype= np.float32)
 
 	def step(self,action):
 
 		# State Update 
 		vx = action[0]
 		vy = action[1]
-		state_robot, warn =self.robot.step(vx,vy)
+		self.robot.step(vx,vy)
+		robot_pos = np.array([self.robot.xr, self.robot.yr])
 		flag_crash = self.robot.is_crashed()
 
 		# Observation Update
-		x_laser_s, y_lases_s = self.robot.scanning()
+		self.robot.scanning()
+		obs = np.array([self.robot.xls, self.robot.yls])
+
 
 		# Done condition
+		done = bool(
+			is_crashed 
+			or np.linalg.norm(self.robot_goal-robot_pos) <= self.eps_err
+			or self.steps == 200
+			)
 
-		reward = self.reward_evaluation()# Reward evaluation - To complete
+		if not done:
+			self.steps += 1
+			if is_crashed: 
+				reward = -100
+			elif: 
+				reward = np.linalg.norm(self.robot_goal-robot_pos)
+		else: 
+			reward = 0
+			# Acabó el número de episodiroos
 
-
-		return np.array(self.state), reward, done, {}
+		return np.concatenate(robot_pos,obs) , reward, done, {}
 
 	def reset(self): # Return to initial state
+		self.robot.reset()
+		print("The environment has been reset")
+		return np.array([self.robot.xr, self.robot.yr])
 
-	def close(self):
+
+	def render(self):
+		self.robot.render()
